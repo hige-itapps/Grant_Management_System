f0ae9a91d2bfa147c583f64ce0530448fdc7e0f1
skison <samuel.j.kison@wmich.edu>
2018-01-17 16:06:17 -0500

    Added v0.1 files

---
+++ upload.php
@@ -0,0 +1,25 @@
+<?php
+	ob_start();
+	try
+	{
+		if(isset($_FILES["file"]))
+		{
+			$file = $_FILES["file"]["tmp_name"];
+			$doc = $_FILES["file"]["name"];
+			$ftp = ftp_connect("codigo-tech.com") or die("Could not connect to $ftp_server");
+			$login = ftp_login($ftp, "egf897jck0fu", "Default1234$");
+			ftp_pasv($ftp, true);
+			if(ftp_put($ftp, "/public_html/" . $doc, $file, FTP_BINARY))
+			{
+				ftp_close($ftp);
+				echo "success";
+			}
+		}else{
+			echo "no file";
+		}
+	}
+	catch(Exception  $e)
+	{
+		echo $e;
+	}
+?>
