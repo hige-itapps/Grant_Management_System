3437e9c1060af01fbedb85224c8779633ee56d18
skison <samuel.j.kison@wmich.edu>
2018-01-22 13:33:00 -0500

    Changed upload.php to send new files to /uploads

--- upload.php
+++ upload.php
@@ -6,7 +6,7 @@
 		{
 			$file = $_FILES["file"]["tmp_name"];
 			$doc = $_FILES["file"]["name"];
-			$ftp = ftp_connect("codigo-tech.com") or die("Could not connect to $ftp_server");
+			$ftp = ftp_connect("codigo-tech.com/uploads") or die("Could not connect to $ftp_server");
 			$login = ftp_login($ftp, "egf897jck0fu", "Default1234$");
 			ftp_pasv($ftp, true);
 			if(ftp_put($ftp, "/public_html/" . $doc, $file, FTP_BINARY))
