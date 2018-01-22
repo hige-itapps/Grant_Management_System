3d010dcc9ad1dbb87e4579c1b0d35ffa32e4d334
skison <samuel.j.kison@wmich.edu>
2018-01-22 15:04:45 -0500

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,25 @@
+<?php
+/* header to be included in every page on the site */
+/* Check session variables to decide what to render in the header (login, signout, etc.) */
+?>
+
+<div class="container-fluid">
+	<div class="page-header">
+		<div class="row">
+			<div class="col-md-4">
+				<center>
+					<p><img src="images/WMU.png" class="logo" /></p>
+					<p class="title">Haenicke Institute for Global Education</p>
+				</center>
+			</div>
+			<div class="col-md-4"> 
+			</div>
+			<div class="col-md-4">
+				<center>
+					<p class="title">Example Login/Signout area</p>
+					<a href="#">Login/Signout</a>
+				</center>
+			</div>
+		</div>
+	</div>
+</div>
