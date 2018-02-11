69e3fd973343a6bd631c2dda363251d597ed6793
olizardo47 <santanalizardo34@gmail.com>
2018-02-11 15:50:16 -0500

    Add files via upload

--- functions/database.php
+++ functions/database.php
@@ -1,35 +1,7 @@
 <?php
 	include "config.php"; //allows us to use configurations
 
-	
-	/* HOLDS INFO ABOUT APPLICANT - SELF EXPLANATORY */
-	class Applicant{
-		public $bnid;
-		public $name;
-		public $dateS;
-		public $dept;
-		public $deptM;
-		public $email;
-		public $rTitle;
-		public $tStart;
-		public $tEnd;
-		public $aStart;
-		public $aEnd;
-		public $dest;
-		public $aReq;
-		public $pr1;
-		public $pr2;
-		public $pr3;
-		public $pr4;
-		public $oF;
-		public $pS;
-		public $fg1;
-		public $fg2;
-		public $fg3;
-		public $fg4;
-		public $deptCE;
-		public $budget;
-	}
+	include_once "applicant.php";
 	
 	/* Establishes an sql connection to the database, and returns the object; MAKE SURE TO SET OBJECT TO NULL WHEN FINISHED */
 	if(!function_exists('connection')) {
