8e136ffdf147f4448178ea3f72aa048eea7a500e
skison <samuel.j.kison@wmich.edu>
2018-02-20 12:40:21 -0500

    updated application.php
    Added check to avoid duplicate definitions

--- include/application.php
+++ include/application.php
@@ -2,32 +2,34 @@
 
 
 	/* HOLDS INFO ABOUT AN APPLICATION - SELF EXPLANATORY */
-	class Application{
-		public $bnid;
-		public $name;
-		public $dateS;
-		public $dept;
-		public $deptM;
-		public $email;
-		public $rTitle;
-		public $tStart;
-		public $tEnd;
-		public $aStart;
-		public $aEnd;
-		public $dest;
-		public $aReq;
-		public $pr1;
-		public $pr2;
-		public $pr3;
-		public $pr4;
-		public $oF;
-		public $pS;
-		public $fg1;
-		public $fg2;
-		public $fg3;
-		public $fg4;
-		public $deptCE;
-		public $budget;
+	if(!class_exists('Application')){
+		class Application{
+			public $bnid;
+			public $name;
+			public $dateS;
+			public $dept;
+			public $deptM;
+			public $email;
+			public $rTitle;
+			public $tStart;
+			public $tEnd;
+			public $aStart;
+			public $aEnd;
+			public $dest;
+			public $aReq;
+			public $pr1;
+			public $pr2;
+			public $pr3;
+			public $pr4;
+			public $oF;
+			public $pS;
+			public $fg1;
+			public $fg2;
+			public $fg3;
+			public $fg4;
+			public $deptCE;
+			public $budget;
+		}
 	}
 
 ?>
