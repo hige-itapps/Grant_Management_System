6d75eb6cd39e4b189014ad5de3e39160c9df7c3e
skison <samuel.j.kison@wmich.edu>
2018-02-26 10:13:09 -0500

    No longer need these
    These vars were used for early debugging, but are now outdated

--- include/validationvariables.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*These are variables we can use on every page to check the user's BroncoNetID and email.
- The actual setting of these variables must happen elsewhere*/
-$broncoNetID = '';
-$email = '';
-$position = ''; //staff, faculty, etc.
-?>
