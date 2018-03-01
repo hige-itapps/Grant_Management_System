c6480b7ddf20804c23d67100ff7e671aa308168a
alan-alvarez <alan-alvarez@users.noreply.github.com>
2018-03-01 15:05:20 -0500

    Added all table editing
    Can now add and remove from all tables (except Applications and Applicants)

--- administrator.php
+++ administrator.php
@@ -1,6 +1,55 @@
 <?php
 	/*database functions*/
 	include "functions/database.php";
+	$conn = connection();
+	
+	/*Debug user validation*/
+	include "include/debugAuthentication.php";
+	
+	/*Verification functions*/
+	include "functions/verification.php";
+	
+	if(isAdministrator($conn, $_SESSION['broncoNetID'])) {
+		
+		if(isset($_GET["addAdminID"]) && isset($_GET["addAdminName"])) {
+			addAdmin($conn, $_GET["addAdminID"], $_GET["addAdminName"]);
+		}
+		
+		// set an application as denied, cannot remove an application ever 
+		/* if(isset($_GET["denyApplicationID"])) {
+			
+			echo 'in denyApplicationID!!';
+			denyApplication($conn, $_GET["denyApplicationID"]);
+			
+		} */
+		
+		if(isset($_GET["addApproverID"]) && isset($_GET["addApproverName"])) {
+			addApplicationApprover($conn, $_GET["addApproverID"], $_GET["addApproverName"]);
+		}
+		
+		if(isset($_GET["removeApproverID"])) {
+			removeApplicationApprover($conn, $_GET["removeApproverID"]);
+		}
+		
+		if(isset($_GET["addCommitteeID"]) && isset($_GET["addCommitteeName"])) {
+			addCommittee($conn, $_GET["addCommitteeID"], $_GET["addCommitteeName"]);
+		}
+		
+		if(isset($_GET["removeCommitteeID"])) {
+			removeCommittee($conn, $_GET["removeCommitteeID"]);
+		}
+		
+		if(isset($_GET["addFollowupID"]) && isset($_GET["addFollowupName"])) {
+			addFollowUpApprover($conn, $_GET["addFollowupID"], $_GET["addFollowupName"]);
+		}
+		
+		if(isset($_GET["removeFollowupID"])) {
+			removeFollowUpApprover($conn, $_GET["removeFollowupID"]);
+		}
+	}
+	
+	
+	
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,6 +86,23 @@
 		<!--HEADER-->
 		<?php
 			include 'include/header.php';
+			
+			if(isset($_GET["removeAdminID"])) {
+				$_removeAdminID = $_GET['removeAdminID'];
+				
+				if($_SESSION['broncoNetID'] != $_removeAdminID) {
+					removeAdmin($conn, $_removeAdminID);
+				} else {
+					?>
+					<script language="javascript">
+					alert("Cannot remove yourself as an admin!")
+					</script>
+			<?php
+				}
+			}
+			
+			if(isAdministrator($conn, $_SESSION['broncoNetID'])) 
+			{ 
 		?>
 		<!--HEADER-->
 	
@@ -48,8 +114,6 @@
 			</center>
 		
 			<?php
-				$conn = connection(); //connect to DB
-				
 				/*get all database content*/
 				$administrators = getAdministrators($conn);
 				$applicants = getApplicants($conn);
@@ -57,9 +121,9 @@
 				$applicationApprovers = getApplicationApprovers($conn);
 				$committee = getCommittee($conn);
 				$followUpReportApprovers = getFollowUpReportApprovers($conn);
-				
-				$conn = null; //close connection
 			?>
+			
+			<!--Admin Table-->
 			<div ng-app="myApp" ng-controller="adminCtrl">
 				<p class="title">Administrators:</p>
 				<table>
@@ -70,12 +134,26 @@
 					<tr ng-repeat="x in admins">
 						<td>{{ x.BroncoNetID }}</td>
 						<td>{{ x.Name }}</td>
+						<td><a href="?removeAdminID={{x.BroncoNetID}}">REMOVE</a></td> 
 					</tr>
 				</table>
 			</div>
+			
+			<!--Add Admin-->
+			<input type="button" id="addAdmin" value="Add">
+			<div id="addAdminContent" style="display:none"> 
+				<form action="/administrator.php" method="GET"> 
+					BroncoNetID: <input type="text" id="addAdminID" name="addAdminID"><br>
+					Name: <input type="text" id="addAdminName" name="addAdminName"><br>
+					<input name="submitAddAdmin" type="submit" value="Submit">
+				</form>
+			</div>
+		
+			
 			<br />
 			<br />
 			
+			<!--Applicant Names Table, should not edit this table at all as Administrator-->
 			<div ng-app="myApp" ng-controller="applicantCtrl">
 				<p class="title">Applicants:</p>
 				<table>
@@ -90,6 +168,7 @@
 			<br />
 			<br />
 			
+			<!-- applications -->
 			<div ng-app="myApp" ng-controller="applicationCtrl">
 				<p class="title">Applications:</p>
 				<table>
@@ -99,15 +178,17 @@
 						<th>Date</th>
 					</tr>
 					<tr ng-repeat="x in applications">
-						<td>{{ x.ID }}</td>
-						<td>{{ x.Applicant }}</td>
-						<td>{{ x.Date }}</td>
+						<td>{{ x.id }}</td>
+						<td>{{ x.name }}</td>
+						<td>{{ x.dateS }}</td>
+						<td><a href="../application_admin.php?id={{ x.id }}">VIEW</a></td> 
 					</tr>
 				</table>
 			</div>
 			<br />
 			<br />
 			
+			<!--Add or remove application approvers-->
 			<div ng-app="myApp" ng-controller="applicationApproverCtrl">
 				<p class="title">Application Approvers:</p>
 				<table>
@@ -118,12 +199,26 @@
 					<tr ng-repeat="x in applicationApprovers">
 						<td>{{ x.BroncoNetID }}</td>
 						<td>{{ x.Name }}</td>
+						<td><a href="?removeApproverID={{x.BroncoNetID}}">REMOVE</a></td> 
 					</tr>
 				</table>
 			</div>
+			
+			<!--Add approver-->
+			<input type="button" id="addApprover" value="Add">
+			<div id="addApproverContent" style="display:none"> 
+				<form action="/administrator.php" method="GET"> 
+					BroncoNetID: <input type="text" id="addApproverID" name="addApproverID"><br>
+					Name: <input type="text" id="addApproverName" name="addApproverName"><br>
+					<input name="submitAddApprover" type="submit" value="Submit">
+				</form>
+			</div>
+			
 			<br />
 			<br />
 			
+			
+			<!--Committee table-->
 			<div ng-app="myApp" ng-controller="committeeCtrl">
 				<p class="title">Committee Members:</p>
 				<table>
@@ -134,12 +229,25 @@
 					<tr ng-repeat="x in committee">
 						<td>{{ x.BroncoNetID }}</td>
 						<td>{{ x.Name }}</td>
+						<td><a href="?removeCommitteeID={{x.BroncoNetID}}">REMOVE</a></td> 
 					</tr>
 				</table>
 			</div>
+			
+			<!--Add committee member-->
+			<input type="button" id="addCommittee" value="Add">
+			<div id="addCommitteeContent" style="display:none"> 
+				<form action="/administrator.php" method="GET"> 
+					BroncoNetID: <input type="text" id="addCommitteeID" name="addCommitteeID"><br>
+					Name: <input type="text" id="addCommitteeName" name="addCommitteeName"><br>
+					<input name="submitAddCommittee" type="submit" value="Submit">
+				</form>
+			</div>
+			
 			<br />
 			<br />
 			
+			<!--Follow-Up Report Approvers-->
 			<div ng-app="myApp" ng-controller="followUpReportApproverCtrl">
 				<p class="title">Follow-Up Report Approvers:</p>
 				<table>
@@ -150,16 +258,68 @@
 					<tr ng-repeat="x in followUpReportApprovers">
 						<td>{{ x.BroncoNetID }}</td>
 						<td>{{ x.Name }}</td>
+						<td><a href="?removeFollowupID={{x.BroncoNetID}}">REMOVE</a></td> 
 					</tr>
 				</table>
+			</div>
+			
+			<!--Add follow-up member-->
+			<input type="button" id="addFollowup" value="Add">
+			<div id="addFollowupContent" style="display:none"> 
+				<form action="/administrator.php" method="GET"> 
+					BroncoNetID: <input type="text" id="addFollowupID" name="addFollowupID"><br>
+					Name: <input type="text" id="addFollowupName" name="addFollowupName"><br>
+					<input name="submitAddFollowup" type="submit" value="Submit">
+				</form>
 			</div>
 			<br />
 			<br />
 			
 		</div>
 		<!--BODY-->
-	
+		<?php
+			}
+			else{
+			?>
+				<h1>You do not have administrative rights!</h1>
+			<?php
+			}
+			?>
 	</body>
+	
+	<!-- JQuery Script  -->
+	<script>
+	
+		/*Function to show/hide Administrator add*/
+		jQuery(document).ready(function(){
+			jQuery('#addAdmin').on('click', function(event) {        
+				jQuery('#addAdminContent').toggle('show');
+			});
+		});
+		
+		/*Function to show/hide Approver add*/
+		jQuery(document).ready(function(){
+			jQuery('#addApprover').on('click', function(event) {        
+				jQuery('#addApproverContent').toggle('show');
+			});
+		});
+		
+		/*Function to show/hide Committee add*/
+		jQuery(document).ready(function(){
+			jQuery('#addCommittee').on('click', function(event) {        
+				jQuery('#addCommitteeContent').toggle('show');
+			});
+		});
+		
+		/*Function to show/hide Follow-Up add*/
+		jQuery(document).ready(function(){
+			jQuery('#addFollowup').on('click', function(event) {        
+				jQuery('#addFollowupContent').toggle('show');
+			});
+		});
+		
+	</script> 
+	<!-- End Script -->
 	
 	<!-- AngularJS Script -->
 	<script>
@@ -198,3 +358,7 @@
 	<!-- End Script -->
 	
 </html>
+
+<?php
+	$conn = null; //close connection
+?>
