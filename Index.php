da2c213bace571d18cffd01167899890fdece26b
skison <samuel.j.kison@wmich.edu>
2018-04-09 11:06:29 -0400

    Removed AngularJS error

--- Index.php
+++ Index.php
@@ -40,7 +40,7 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	
 	</head>
-	<body ng-app="HIGE-app">
+	<body>
 	
 		<!--HEADER-->
 		<?php
