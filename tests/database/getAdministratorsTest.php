d557d122a62c60b8f4f82b853ec82b5e02c07324
skison <samuel.j.kison@wmich.edu>
2018-05-17 08:24:25 -0400

    Almost got database testing working

--- tests/database/getAdministratorsTest.php
+++ tests/database/getAdministratorsTest.php
@@ -9,20 +9,36 @@ final class getAdministratorsTest extends TestCase
 {    
     use TestCaseTrait;
 
+    // only instantiate pdo once for test clean-up/fixture load
+    static private $pdo = null;
+    // only instantiate PHPUnit\DbUnit\Database\Connection once per test
+    private $conn = null;
+   
+
     /**
      * @return PHPUnit\DbUnit\Database\Connection
      */
     public function getConnection()
     {
+        //Connection information
         $config_url = dirname(__FILE__).'/../testconfig.ini';
         $settings = parse_ini_file($config_url);
-        
-        //var_dump($settings);
-        $conn = new PDO("mysql:host=" . $settings["test_hostname"] . ";dbname=" . $settings["test_database_name"] . ";charset=utf8", $settings["test_database_username"], 
-            $settings["test_database_password"], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $conn->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
+
+        if ($this->conn === null) {
+            if (self::$pdo == null) {
+                self::$pdo = new PDO("mysql:host=" . $settings["test_hostname"] . ";dbname=" . $settings["test_database_name"] . ";charset=utf8", $settings["test_database_username"], 
+                    $settings["test_database_password"], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+                // set the PDO error mode to exception
+                //self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //self::$pdo->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
+            }
+            //$this->conn = $this->createDefaultDBConnection(self::$pdo, $settings["test_database_name"]);
+            $this->conn = $this->createDefaultDBConnection(self::$pdo);
+
+            //$conn = new PDO("mysql:host=" . $settings["test_hostname"] . ";dbname=" . $settings["test_database_name"] . ";charset=utf8", $settings["test_database_username"], $settings["test_database_password"], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        }
+
+        return $this->conn;
     }
 
     /**
@@ -30,7 +46,7 @@ final class getAdministratorsTest extends TestCase
      */
     public function getDataSet()
     {
-        return $this->createFlatXMLDataSet(dirname(__FILE__).'/_files/guestbook-seed.xml');
+        return $this->createFlatXMLDataSet(dirname(__FILE__).'/datasets/administrators2.xml');
     }
 
 
